6f1511a2e8b7d719eb0b2ddcdefee6782b8419f0
Jan Vejnárek <Veejnny@gmail.com>
2017-09-21 11:04:40 +0200

    Ah

--- Ships/Ships/Program.cs
+++ Ships/Ships/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace Ships
 {
+    enum Typy
+    {
+        Lod, Prazdno, Strela, Minuto
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            AhojxDAHOJ
+
+            Typy moznost = Typy.Prazdno;
+            Typy[] more = new Typy[3];
+            more[0] = Typy.Lod;
+            more[1] = Typy.Strela;
+            more[2] = Typy.Prazdno;
         }
     }
 }
